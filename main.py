c5e7533edb922ad0d4db751be1e46f71df526f17
Vibhor2003 <77459342+Vibhor2003@users.noreply.github.com>
2021-03-07 16:25:26 +0530

    Embed

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import discord
+from discord.ext import commands
+import random
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-client = discord.Client()
+images = [
+'https://i.imgur.com/JQGhevZ.jpg',
+'https://i.imgur.com/yl4U0ZY.jpg',
+'https://i.imgur.com/NuDbYN6.jpg',
+'https://i.imgur.com/OGwJeAX.jpg',
+'https://i.imgur.com/IFcT93v.jpg',
+'https://i.imgur.com/WAjMADM.jpg',
+'https://i.imgur.com/6AiIwzp.jpg',
+'https://i.imgur.com/SZWbVzh.jpg',
+'https://i.imgur.com/LWMk1N3.jpg',
+'https://i.imgur.com/VCmy1lp.jpg',
+'https://i.imgur.com/UIHtWjS.jpg',
+'https://i.imgur.com/9iUeAKt.jpg',
+'https://i.imgur.com/42IdSrp.jpg',
+'https://i.imgur.com/YYHB9Vm.jpg']
+
+client = commands.Bot(command_prefix="-")
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def hello(ctx):
+    await ctx.send("hi")
 
-    msg = message.content.split()
-    searchQuery = ""
-    #edit this to fetch and display search results
-    if msg[0] == "-search":
-        for i in range(1, len(msg)):
-            searchQuery = searchQuery + (msg[i] + " ")
-        await message.channel.send("You want to search: " + searchQuery)
+@client.command(aliases=['user','info'])
+@commands.has_permissions(kick_members=True)
+async def whois(ctx,member:discord.Member):
+    embed = discord.Embed(title = member.name, description = member.mention, color = discord.Colour.red())
+    embed.add_field(name = "ID",value = member.id,inline = True)
+    embed.set_thumbnail(url = member.avatar_url)
+    await ctx.send(embed=embed)
+
+@client.command()
+async def meme(ctx):
+    embed = discord.Embed(color = discord.Colour.green())
+    random_link = random.choice(images)
+    embed.set_image(url = random_link)
+    await ctx.send(embed=embed)
 
 client.run(os.getenv('TOKEN'))
